3fb9a3e2c86cf592dcf4f26748f61b162359530f
Deniska Asus <k.denis.vlad@gmail.com>
2020-11-05 09:25:50 +0200

    2ая лекция; создаю первый объект и вызываю бин скалаДев; сетаю ему скил Делфи; потом создаю второй объект и печатаю его; первый и второй объекты имеют одинаковую ссылку; при вызове повторно бина второй раз я уже получаю просто ссылку на тот же объект который был создан первым. И если первый объект был уже изменен, то при повторном вызове бина скалаДев я получаю не бин из xml, а измененный объект; таким образом у меня 2 переменные с одианковой ссулкой на один и тот же объект; это типо scope singleton (значение по умолчанию)

--- src/main/java/beanDefinition/ClientCode.java
+++ src/main/java/beanDefinition/ClientCode.java
@@ -8,25 +8,35 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ClientCode {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        Developer dev = (Developer) context.getBean("dev");
-        System.out.println(dev.toString());
+//        Developer dev = (Developer) context.getBean("dev");
+//        System.out.println(dev.toString());
+//
+//        Developer intern = context.getBean("intern", Developer.class);
+//        intern.setLevel(1);
+//        intern.setSkill("Kotlin");
+//        System.out.println(intern.toString());
+//
+//        Developer net = (Developer) context.getBean("dotNetDev");
+//        System.out.println(net.toString());
+//
+//        Developer anonymous = context.getBean("anonymous", Developer.class);
+//        System.out.println(anonymous.toString());
+//
+//        Project taxiBumer = context.getBean("taxiBumer", Project.class);
+//        System.out.println(taxiBumer.toString());
+//
+//        Project java_10 = (Project) context.getBean("java_10");
+//        System.out.println(java_10.toString());
 
-        Developer intern = context.getBean("intern", Developer.class);
-        intern.setLevel(1);
-        intern.setSkill("Kotlin");
+        /*2ая лекция*/
+        Developer intern = context.getBean("scalaDev", Developer.class);
+        intern.setSkill("Delphi");
         System.out.println(intern.toString());
 
-        Developer net = (Developer) context.getBean("dotNetDev");
-        System.out.println(net.toString());
+        Developer scalaDev = (Developer) context.getBean("scalaDev");
+        System.out.println(scalaDev.toString());
 
-        Developer anonymous = context.getBean("anonymous", Developer.class);
-        System.out.println(anonymous.toString());
-
-        Project taxiBumer = context.getBean("taxiBumer", Project.class);
-        System.out.println(taxiBumer.toString());
-
-        Project java_10 = (Project) context.getBean("java_10");
-        System.out.println(java_10.toString());
+        System.out.println(intern==scalaDev);
 
     }
 }
