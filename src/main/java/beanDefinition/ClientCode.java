618ab2241c9f1a4d19886f8d40042923e053bd2b
Deniska Asus <k.denis.vlad@gmail.com>
2020-11-05 10:56:40 +0200

    делаю класс Engineer.java с полями name, skill, level; от него наследую Developer'а с приватной мапой по годам-в-фреймворке и буленовскому кофепотреблению; в xml прописываю проперти и мапу; вывожу в консоль

--- src/main/java/beanDefinition/ClientCode.java
+++ src/main/java/beanDefinition/ClientCode.java
@@ -1,45 +1,15 @@
 package beanDefinition;
 
 import beans.Developer;
-import beans.Project;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ClientCode {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-//        Developer dev = (Developer) context.getBean("dev");
-//        System.out.println(dev.toString());
-//
-//        Developer intern = context.getBean("intern", Developer.class);
-//        intern.setLevel(1);
-//        intern.setSkill("Kotlin");
-//        System.out.println(intern.toString());
-//
-//        Developer net = (Developer) context.getBean("dotNetDev");
-//        System.out.println(net.toString());
-//
-//        Developer anonymous = context.getBean("anonymous", Developer.class);
-//        System.out.println(anonymous.toString());
-//
-//        Project taxiBumer = context.getBean("taxiBumer", Project.class);
-//        System.out.println(taxiBumer.toString());
-//
-//        Project java_10 = (Project) context.getBean("java_10");
-//        System.out.println(java_10.toString());
 
         /*2ая лекция*/
-        Developer intern = context.getBean("scalaDev", Developer.class);
-        intern.setSkill("Delphi");
+        Developer intern = context.getBean("dev", Developer.class);
         System.out.println(intern.toString());
-
-
-        ApplicationContext context2 = new ClassPathXmlApplicationContext("Beans.xml");
-
-        Developer scalaDev = (Developer) context2.getBean("scalaDev");
-        System.out.println(scalaDev.toString());
-
-        System.out.println(intern==scalaDev);
-
     }
 }
--- src/main/java/beans/Developer.java
+++ src/main/java/beans/Developer.java
@@ -1,51 +1,20 @@
 package beans;
 
-public class Developer {
-    private String name;
-    private String skill;
-    private int level;
-    private boolean isCoffeeConsumer;
+import java.util.Map;
 
-    public Developer(String name, String skill, int level, boolean isCoffeeConsumer) {
-        this.name = name;
-        this.skill = skill;
-        this.level = level;
-        this.isCoffeeConsumer = isCoffeeConsumer;
-    }
+public class Developer extends Engineer {
+    private Map<String, Integer> yearsInFramework;
+    private boolean isCoffeeConsumer;
 
     public Developer() {
     }
 
-    public Developer(String name) {
-        this.name = name;
+    public Map<String, Integer> getYearsInFramework() {
+        return yearsInFramework;
     }
 
-    public Developer(int level) {
-        this.level = level;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSkill() {
-        return skill;
-    }
-
-    public void setSkill(String skill) {
-        this.skill = skill;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
+    public void setYearsInFramework(Map<String, Integer> yearsInFramework) {
+        this.yearsInFramework = yearsInFramework;
     }
 
     public boolean isCoffeeConsumer() {
@@ -59,10 +28,8 @@ public class Developer {
     @Override
     public String toString() {
         return "Developer{" +
-                "name='" + name + '\'' +
-                ", skill='" + skill + '\'' +
-                ", level=" + level +
+                "yearsInFramework=" + yearsInFramework +
                 ", isCoffeeConsumer=" + isCoffeeConsumer +
-                '}';
+                "} " + super.toString();
     }
 }
