0e500a8ca717ad35cb7626ef05f081c0158c60e1
Deniska Asus <k.denis.vlad@gmail.com>
2020-11-06 10:34:27 +0200

    теперь можно вмешиваться в порядок обработки бина до его инициализации, например вернуть null, если объект Project

--- src/main/java/beanDefinition/CustomBPP.java
+++ src/main/java/beanDefinition/CustomBPP.java
@@ -1,11 +1,15 @@
 package beanDefinition;
 
+import beans.Project;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 public class CustomBPP implements BeanPostProcessor {
     public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
         System.out.println("Bean " + s + " START");
+        if (o instanceof Project){
+            return null;
+        }
         return o;
     }
 
