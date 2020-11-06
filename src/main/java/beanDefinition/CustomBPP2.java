4caea3e3701a2f6ce9f522610f0cbc4972dce05f
Deniska Asus <k.denis.vlad@gmail.com>
2020-11-06 10:48:56 +0200

    можно иметь несколько BeanPostProcessors

--- src/main/java/beanDefinition/CustomBPP2.java
+++ src/main/java/beanDefinition/CustomBPP2.java
@@ -3,14 +3,14 @@ package beanDefinition;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
-public class CustomBPP implements BeanPostProcessor {
+public class CustomBPP2 implements BeanPostProcessor {
     public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
-        System.out.println("Bean " + s + " START");
+        System.out.println("Bean2 " + s + " START");
         return o;
     }
 
     public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
-        System.out.println("Bean " + s + " END");
+        System.out.println("Bean2 " + s + " END");
         return o;
     }
 }
